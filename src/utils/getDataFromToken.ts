52bf867d5cac16e5edd26e74fd1dd518e63fd37e
git-demo2 <vaibhavahir4456@gmail.com>
2024-11-16 23:23:54 +0530

    solve ts error

--- src/utils/getDataFromToken.ts
+++ src/utils/getDataFromToken.ts
@@ -1,5 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
+
+interface DecodedToken {
+  userId: string; // Replace with the actual fields in your token payload
+  [key: string]: unknown; // Optional: if the token may include additional fields
+}
 export const getData = (req: NextRequest) => {
   try {
     const token = req.cookies.get("token")?.value || "";
@@ -10,7 +15,7 @@ export const getData = (req: NextRequest) => {
       });
     }
 
-    const decodedToken: any = jwt.verify(token, process.env.jwt_secret!);
+    const decodedToken: any = jwt.verify(token, process.env.jwt_secret!) as DecodedToken;
     return decodedToken.userId;
   } catch (error) {
     console.log("error while getting data from token", error);
