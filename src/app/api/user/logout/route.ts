5c87f773e57e207a6e410629c5c1279a892fa18b
git-demo2 <vaibhavahir4456@gmail.com>
2024-11-16 22:46:48 +0530

    logout user

---
+++ src/app/api/user/logout/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  try {
+    const res = NextResponse.json({
+      message: "User logged out successfully",
+      status: 200,
+    });
+
+    res.cookies.set("token", "", {
+      expires: new Date(0), // Set the cookie to expire immediately
+      path: "/",
+      sameSite: "strict",
+      secure: true,
+      httpOnly: true,
+      maxAge: 0,
+    });
+
+    return res;
+  } catch (error) {
+    console.log("error while logout user", error);
+    return NextResponse.json(
+      { error: "error while logout user" },
+      { status: 500 }
+    );
+  }
+}
