f3632b25925e3627e2ee28170a8d4c731592a4b0
git-demo2 <vaibhavahir4456@gmail.com>
2024-12-04 11:13:41 +0530

    solve error

--- src/app/api/component/create-review/route.ts
+++ src/app/api/component/create-review/route.ts
@@ -2,6 +2,14 @@
 
 import { NextRequest } from "next/server";
 
+interface ReviewProps{
+    rating: number;
+    comment: string;
+    userId: string;
+}
+
 export async function name(request:NextRequest) {
+    const { rating , comment } : ReviewProps = await request.json();
+    console.log(rating , comment);
     
 }
